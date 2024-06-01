9d97e8b81383953a69b8bd4a86ca22ccf74ba8a9
pznamir00 <pznamir00@gmail.com>
2024-06-01 16:01:11 +0200

    ci(): add jest and eslint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('stats-page');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'stats-page app is running!',
-    );
-  });
 });
