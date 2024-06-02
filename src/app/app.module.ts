819b2c3e346060d4af12cbfd80f1066d5d07aa6b
pznamir00 <pznamir00@gmail.com>
2024-06-02 16:42:36 +0200

    style(app): add nebular and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { HttpClientModule } from "@angular/common/http";
 import { ChartComponent } from "./chart/chart.component";
 import { NgChartsModule } from "ng2-charts";
 import { FiltersComponent } from "./filters/filters.component";
+import { NbCardModule, NbLayoutModule, NbThemeModule } from "@nebular/theme";
 
 @NgModule({
   declarations: [AppComponent, ChartComponent, FiltersComponent],
-  imports: [BrowserModule, HttpClientModule, NgChartsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    NgChartsModule,
+    NbLayoutModule,
+    NbThemeModule.forRoot(),
+    NbCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
