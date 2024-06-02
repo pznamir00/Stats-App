4740dfebf621b9fd3937a73c7fada157146aa42c
pznamir00 <pznamir00@gmail.com>
2024-06-02 18:48:01 +0200

    test(chart.comp): add tests

--- src/app/chart/chart.component.spec.ts
+++ src/app/chart/chart.component.spec.ts
@@ -1,27 +1,50 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { By } from "@angular/platform-browser";
-import { DebugElement } from "@angular/core";
-
 import { ChartComponent } from "./chart.component";
+import { render, screen } from "@testing-library/angular";
+import { EventRecord } from "../types/event.model";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 
 describe("ChartComponent", () => {
-  let component: ChartComponent;
-  let fixture: ComponentFixture<ChartComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ChartComponent],
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ChartComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it("shows chart", async () => {
+    await setup();
+    const chart = screen.queryByTestId("chart");
+    expect(chart).toBeTruthy();
   });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
+  it("calculates default data", async () => {
+    const { fixture } = await setup();
+    expect(fixture.fixture.componentInstance.data).toEqual([
+      { data: [2, 2, 0], label: "ios" },
+    ]);
   });
+
+  it("updates data when inputs change", async () => {
+    const { fixture } = await setup();
+    fixture.change({ groupingProperty: "version" });
+    expect(fixture.fixture.componentInstance.data).toEqual([
+      { data: [2, 2, 0], label: "1.44" },
+    ]);
+  });
+});
+
+const setup = async () => {
+  const fixture = await render(ChartComponent, {
+    componentProperties: {
+      eventsByDistinctNames: {
+        start: [getEvent()],
+        interaction: [getEvent()],
+        shop_click: [],
+      },
+      groupingProperty: "platform",
+    },
+    schemas: [NO_ERRORS_SCHEMA],
+  });
+
+  return { fixture };
+};
+
+const getEvent = (): EventRecord => ({
+  count: 2,
+  platform: "ios",
+  version: "1.44",
+  name: "start",
 });
